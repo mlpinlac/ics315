9252767ab32853bcf75a7b4bb5e77c22b82d4731
mlpinlac <mlpinlac@hawaii.edu>
2015-12-06 01:22:02 -1000

    Added home page and genres page
    And other stuff

---
+++ js/customscript.js
@@ -0,0 +1,25 @@
+
+/*
+  @author: Michelle Pinlac
+ */
+$(document).ready(function(){
+
+  // this is the jQuery for lightgenre.html
+  $("#zumbalight").click(function(){
+    window.location = 'lightzumba.html';
+  });
+
+  $("#salsalight").click(function(){
+    window.location = 'lightsalsa.html';
+  });
+
+  $("#linedancelight").click(function(){
+    window.location = 'lightlinedance.html';
+  });
+
+  $("#ballroomlight").click(function(){
+    window.location = 'lightballroom.html';
+  });
+
+});
+
